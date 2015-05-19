4fdcf4b0bc9868f6615d4eeb27dd45cc66226acd
Vlad Lazu <vlad.lazu@gmail.com>
2015-05-19 22:19:26 +0300

    Sign up with ajax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+
+  def twitterized_type(type)
+    case type
+      when :errors
+        "alert-error"
+      when :alert
+        "alert-warning"
+      when :error
+        "alert-error"
+      when :notice
+        "alert-success"
+      else
+        "alert-info"
+    end
+  end
+
+  def flash_normal
+    render "shared/flashes"
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
