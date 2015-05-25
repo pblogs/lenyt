193c7c0fd77a0eabfd1a784248ace65eb10e9ee6
Rosca Sergiu <rmagnum2002@gmail.com>
2015-05-26 02:12:39 +0300

    load elements in slider based on screen size
    let padding around search box

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -16,6 +16,14 @@ jQuery(document).ready(function($) {
   $(".owl-carousel").owlCarousel({
     items : 5,
     lazyLoad : true,
-    navigation : true
+    navigation : true,
+    itemsCustom : false,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,2],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false
   });
 });
