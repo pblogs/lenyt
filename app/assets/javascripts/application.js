43bef6ea5be7abc77d056643893969777165258c
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2015-06-02 15:24:01 +0300

    Init frontend with webpack, preparing for heroku.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// = require lenyt-app
 // = require jquery
 // = require jquery-ui
 // = require jquery_ujs
