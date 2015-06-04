7e4767785ea406502566a1b32cdda4df2173213d
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2015-06-04 17:34:13 +0300

    Pagination FTW!

--- frontend/javascripts/pages/search/onload.js
+++ frontend/javascripts/pages/search/onload.js
@@ -1,8 +1,36 @@
-/* global google */
 var Vue = require('vue')
 var getJson = require('../../services/get-json')
+var gMap = require('../../services/map')
 var productMarkers = []
-var map
+var map = gMap.create(document.getElementById('search_map'), gMap.coords(44.099421, -79.793701))
+var gettingProducts = false
+var page = 1
+
+var getProducts = function (page) {
+  gettingProducts = true
+  getJson('/api/products.json?page=' + page, function (err, prods) {
+    if (err) {
+      return console.error(err)
+    }
+    var template = document.getElementsByClassName('map-pointi')[0].innerHTML
+
+    prods.products.forEach(function (product, index) {
+      var options = {
+        content: template.replace(/IMAGE_URL/g, product.image).replace(/TITLE/g, product.title).replace(/PRICE/g, '$' + product.price_per_day),
+        position: gMap.coords(product.map.lat, product.map.long)
+      }
+      var iwindow = gMap.infoWindow(map, options).open().onClose(function () {
+        v.$data.products[index].active = false
+      })
+      productMarkers.push(iwindow)
+      product.active = true
+      v.$data.products.push(product)
+    })
+    if (prods.products.length) {
+      gettingProducts = false
+    }
+  })
+}
 
 var v = new Vue({
   el: '#lenyt-search',
@@ -22,34 +50,13 @@ var v = new Vue({
       } else {
         productMarkers[index].close()
       }
+    },
+    scroll: function (e) {
+      if (e.target.scrollTop + 700 > e.target.scrollHeight && !gettingProducts) {
+        getProducts(++page)
+      }
     }
   }
 })
 
-getJson('/api/products.json', function (err, prods) {
-  if (err) {
-    return console.error(err)
-  }
-  var template = document.getElementsByClassName('map-pointi')[0].innerHTML
-
-  map = new google.maps.Map(document.getElementById('search_map'), {
-    zoom: 8,
-    center: new google.maps.LatLng(44.099421, -79.793701)
-  })
-
-  prods.products.forEach(function (product, index) {
-    var options = {
-      content: template.replace(/IMAGE_URL/g, product.image).replace(/TITLE/g, product.title).replace(/PRICE/g, '$' + product.price_per_day),
-      position: new google.maps.LatLng(product.map.lat, product.map.long)
-    }
-    var iwindow = new google.maps.InfoWindow(options)
-    iwindow.open(map)
-    google.maps.event.addListener(iwindow, 'closeclick', function () {
-      v.$data.products[index].active = false
-    })
-    productMarkers.push(iwindow)
-    product.active = true
-  })
-
-  v.$data.products = prods.products
-})
+getProducts(page)
