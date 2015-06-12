a90adf87f7809888553a0c2c7571a038da95ea07
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2015-06-12 23:38:21 +0300

    Calculator.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,38 +1,13 @@
 $(function(){
-  var nowTemp = new Date();
-  var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-
-  var checkin = $('#dpd1').datepicker({
-    onRender: function(date) {
-      return date.valueOf() < now.valueOf() ? 'disabled' : '';
-    }
-  }).on('changeDate', function(ev) {
-      if (ev.date.valueOf() > checkout.date.valueOf()) {
-        var newDate = new Date(ev.date)
-        newDate.setDate(newDate.getDate() + 1);
-        checkout.setValue(newDate);
-      }
-      checkin.hide();
-      $('#dpd2')[0].focus();
-    }).data('datepicker');
-  var checkout = $('#dpd2').datepicker({
-    onRender: function(date) {
-      return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
-    }
-  }).on('changeDate', function(ev) {
-      checkout.hide();
-    }).data('datepicker');
-})
-
-$(function(){
   $("#range").slider({});
 });
 
 $(document).ready(function(){
   $('[data-toggle="popover"]').popover();
-  $("#tchat").mCustomScrollbar();
+  var tChat = $("#tchat")
+  tChat.mCustomScrollbar();
 
-  $("#tchat").mCustomScrollbar("scrollTo","bottom",{
+  tChat.mCustomScrollbar("scrollTo","bottom",{
     scrollEasing:"easeOut"
   });
 });
