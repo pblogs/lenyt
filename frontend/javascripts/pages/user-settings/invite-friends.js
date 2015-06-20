8905914570c2afd683cef6600156fe5d20b37a29
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2015-06-21 01:31:29 +0300

    Primitive multiple mails.

--- frontend/javascripts/pages/user-settings/invite-friends.js
+++ frontend/javascripts/pages/user-settings/invite-friends.js
@@ -10,10 +10,51 @@ var v = new Vue({
   },
   methods: {
     switchOnComma: function (e) {
+      console.log(e.keyCode)
       if (e.keyCode === 188) {
-        v.$data.mails.push(v.$data.mail.replace(/\s|,/g, ''))
+        v.$data.mails.push({
+          value: v.$data.mail.replace(/\s|,/g, ''),
+          status: 0
+        })
         v.$data.mail = ''
       }
+    },
+    removeMail: function (index) {
+      v.$data.mails.splice(index, 1)
+    },
+    sendMails: function (e) {
+      e.preventDefault()
+
+      v.$data.mails.push({
+        value: v.$data.mail.replace(/\s|,/g, ''),
+        status: 0
+      })
+      v.$data.mail = ''
+      v.$data.mails.forEach(function (mail) {
+        request({
+          method: 'POST',
+          uri: '/users/invitation',
+          headers: {
+            'X-Csrf-Token': csrfToken
+          },
+          json: {
+            utf8: '✓',
+            user: {
+              email: mail.value
+            }
+          }
+        }, function (err, response, data) {
+          if (err) {
+            throw err
+          }
+
+          if (data.success) {
+            mail.status = 1
+          } else {
+            mail.status = -1
+          }
+        })
+      })
     }
   }
 })
